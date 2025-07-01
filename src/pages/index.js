1fc7ab0ce3186f1ed8ff379d3fc27195e0094010
Geng Yue <gengyue2468@outlook.com>
2025-07-01 18:21:30 +0800

    prettier

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
-import Layout from '@/components/Layout';
-import moment from 'moment';
-import HomeSkeleton from '@/components/Home-skeleton';
-import { TextScramble } from '@/components/ui/text-scramble';
-import { TextEffect } from '@/components/ui/text-effect';
-import Error from '@/components/Error'
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Layout from "@/components/Layout";
+import moment from "moment";
+import HomeSkeleton from "@/components/Home-skeleton";
+import { TextScramble } from "@/components/ui/text-scramble";
+import { TextEffect } from "@/components/ui/text-effect";
+import Error from "@/components/Error";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
@@ -15,12 +15,15 @@ const Home = () => {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const res = await fetch('/api/notion');
-        if (!res.ok) throw new Error('Failed to Fetch Posts, Please Check Your Internet Connection.');
+        const res = await fetch("/api/notion");
+        if (!res.ok)
+          throw new Error(
+            "Failed to Fetch Posts, Please Check Your Internet Connection."
+          );
 
         const data = await res.json();
         setPosts(data);
-        console.log(data)
+        console.log(data);
       } catch (error) {
         setError(error.message);
       } finally {
@@ -33,32 +36,42 @@ const Home = () => {
 
   return (
     <Layout title="Geng Yue">
-      <div className='mb-8'>
-       <TextScramble as="h1" className="font-bold mb-2.5">Geng Yue</TextScramble>
-        <TextScramble as='p' className='opacity-75'>Hey👋! I'm a senior school graduate who is about to go to university.
-          Here is my online garden and laboratory.</TextScramble>
+      <div className="mb-8">
+        <TextScramble as="h1" className="font-bold mb-2.5">
+          Geng Yue
+        </TextScramble>
+        <TextScramble as="p" className="opacity-75">
+          Hey👋! I'm a senior school graduate who is about to go to university.
+          Here is my online garden and laboratory.
+        </TextScramble>
       </div>
 
-      <div className=''>
-          <TextScramble as="h1" className="font-bold mb-2.5">Recent Published</TextScramble>
-        
-        {error &&
-          <Error error={error} />
-        }
-        {loading && Array.from({ length: 6 }).map((_, index) => (
-          <HomeSkeleton index={index} />
-        ))}
+      <div className="">
+        <TextScramble as="h1" className="font-bold mb-2.5">
+          Recent Published
+        </TextScramble>
 
-        <div className='flex flex-col space-y-2'>
+        {error && <Error error={error} />}
+        {loading &&
+          Array.from({ length: 6 }).map((_, index) => (
+            <HomeSkeleton index={index} />
+          ))}
+
+        <div className="flex flex-col space-y-2">
           {posts.map((post) => (
-            <div key={post.id} className="w-full mb-2 transition-all duration-500 opacity-75 hover:opacity-100">
+            <div
+              key={post.id}
+              className="w-full mb-2 transition-all duration-500 opacity-75 hover:opacity-100"
+            >
               <Link href={`/${post.id}`}>
-                <div className='flex flex-row justify-between items-center space-x-8'>
+                <div className="flex flex-row justify-between items-center space-x-8">
                   <TextScramble as="h1" className="font-semibold">
-                    {post.properties.Title.title[0]?.plain_text || 'Untitled'}
+                    {post.properties.Title.title[0]?.plain_text || "Untitled"}
                   </TextScramble>
                   <TextScramble as="h2" className="font-medium opacity-75 ">
-                    {moment(post.properties.Date?.date?.start).format('MMM DD, YYYY')}
+                    {moment(post.properties.Date?.date?.start).format(
+                      "MMM DD, YYYY"
+                    )}
                   </TextScramble>
                 </div>
               </Link>
@@ -66,7 +79,6 @@ const Home = () => {
           ))}
         </div>
       </div>
-
     </Layout>
   );
 };
