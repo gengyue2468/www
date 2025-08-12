e032d7b6300e8a06db854ee02966c595d74f0b82
Geng Yue <gengyue2468@outlook.com>
2025-08-12 18:06:13 +0800

    fix

--- src/lib/markdown/config.js
+++ src/lib/markdown/config.js
@@ -1,28 +1,6 @@
 import FileDisplay from "@/components/FileDisplay";
 import Image from "@/components/Image";
 import NextLink from "@/components/NextLink";
-import { useTheme } from "next-themes";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import {
-  vs,
-  vscDarkPlus,
-} from "react-syntax-highlighter/dist/esm/styles/prism";
-
-const CodeBlock = ({ children, className }) => {
-  const language = className?.replace(/language-/, "") || "text";
-  const { resolvedTheme } = useTheme();
-
-  return (
-    <SyntaxHighlighter
-      style={resolvedTheme === "light" ? vs : vscDarkPlus}
-      language={language}
-      PreTag="div"
-      className="p-4 rounded-lg overflow-x-auto"
-    >
-      {children}
-    </SyntaxHighlighter>
-  );
-};
 
 const components = {
   img: Image,
