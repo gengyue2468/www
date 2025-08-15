cf56af92cb1a0f88a46c8a4e814f8de1b2a82e07
Geng Yue <gengyue2468@outlook.com>
2025-08-15 09:16:14 +0800

    fix edgeone deploy

--- next.config.mjs
+++ next.config.mjs
@@ -2,30 +2,10 @@
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
-
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    unoptimized: true,
   },
-
-  async rewrites() {
-    return [
-      {
-        source: "/fonts/:path*",
-        destination: `${process.env.NEXT_PUBLIC_CDN_URL}/fonts/:path*`,
-      },
-    ];
-  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
