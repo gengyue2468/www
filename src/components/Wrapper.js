235fb80b36b62ee80e9df330e164cb79faf7339e
Geng Yue <gengyue2468@outlook.com>
2025-08-16 16:35:43 +0800

    slim adjustment

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -6,9 +6,9 @@ export default function Wrapper({ children }) {
     prose-headings:text-lg sm:prose-headings:text-xl prose-headings:my-12 prose-headings:font-semibold 
     prose-pre:mono prose-a:underline-offset-4 
     *:transition-all *:duration-300 
-    prose-li:pl-0 prose-li:[&::marker]:mr-0.5 prose-li:[&::marker]:text-black dark:prose-li:[&::marker]:text-white 
+    prose-li:pl-0 prose-li:[&::marker]:pl-0.5 prose-li:[&::marker]:text-black dark:prose-li:[&::marker]:text-white 
     prose-blockquote:border-l-black dark:prose-blockquote:border-l-white prose-blockquote:-translate-x-5  prose-blockquote:not-italic prose-blockquote:font-medium
-    prose-li:text-black dark:prose-li:text-white prose-li:-translate-x-7 prose-li:opacity-100! prose-li:[&::marker]:font-medium
+    prose-li:text-black dark:prose-li:text-white prose-li:-translate-x-7 prose-li:opacity-100! prose-li:[&::marker]:font-medium prose-li:[&::marker]:font-mono
     prose-strong:font-medium
     prose-pre:rounded-none sm:prose-pre:rounded-xl prose-pre:bg-white dark:prose-pre:bg-black prose-pre:border prose-pre:border-neutral-200 dark:prose-pre:border-neutral-800 prose-code:text-black! dark:prose-code:text-white! prose-pre:-translate-x-8 prose-pre:w-[calc(100%+4rem)]! prose-pre:overflow-x-auto"
     >
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 export default {
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ["Jetbrains Mono Variable", "monospace"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
